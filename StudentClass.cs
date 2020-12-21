bfddfb1e91344e2052b4886a8c05f09b752bd1c4
priyamodak <67309489+priyamodak@users.noreply.github.com>
2020-12-21 06:58:30 -0500

    Add files via upload

---
+++ StudentClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_EF_CRUD_PostgreSQL.Models
+{
+    [Table("Student", Schema = "public")]
+    public class StudentClass
+    {
+        [Key]
+        public int id { get; set; }
+        public string studentname { get; set; }
+        public string email { get; set; }
+
+    }
+}
